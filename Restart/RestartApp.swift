41dad606f5d36fd33db2c924c2cd1b0b617c110b
Yashwant Das <yashworks@gmail.com>
2022-02-02 13:17:36 +0530

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Yashwant Das on 02/02/22.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
