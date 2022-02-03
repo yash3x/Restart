8763c26e92d3f66df013d38fb9d43886ceed189c
Yashwant Das <yashworks@gmail.com>
2022-02-03 08:18:54 +0530

    Update OnboardingView

--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -15,19 +15,39 @@ struct OnboardingView: View {
     //MARK: - BODY
     
     var body: some View {
-        VStack (spacing: 20){
-            Text("Onboarding")
-                .font(.largeTitle)
-            
-            Button(action: {
-                isOnboardingViewActive = false
-            }) {
-                Text("Start")
-            }
-        } //: VSTACK
+        ZStack {
+            Color("ColorBlue")
+                .ignoresSafeArea()
+            VStack (spacing: 20){
+                // MARK: - HEADER
+                Spacer()
+                
+                VStack(spacing: 0){
+                    Text("Share.")
+                        .font(.system(size: 60))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                    
+                    Text("""
+                    It's not much we give but
+                    how much love we put into giving.
+                    """)
+                        .font(.title3)
+                        .fontWeight(.light)
+                        .foregroundColor(.white)
+                        .multilineTextAlignment(.center)
+                        .padding(.horizontal, 10)
+                }//: HEADER
+                
+                // MARK: - CENTER
+                
+                // MARK: - FOOTER
+            } //: VSTACK
+        } //: ZSTACK
     }
 }
 
+// MARK: - PREVIEW
 struct OnboardingView_Previews: PreviewProvider {
     static var previews: some View {
         OnboardingView()
