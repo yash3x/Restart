d7845ad58a675870439318f3d56202c5832b0c27
Yashwant Das <yashworks@gmail.com>
2022-02-06 21:23:08 +0530

    Add Complex SwiftUI Drag Gestures

--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -12,6 +12,10 @@ struct OnboardingView: View {
     
     @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
     
+    @State private var buttonWidth: Double = UIScreen.main.bounds.width - 80
+    @State private var buttonOffset: CGFloat = 0
+    
+    
     //MARK: - BODY
     
     var body: some View {
@@ -59,6 +63,7 @@ struct OnboardingView: View {
                     
                     Capsule()
                         .fill(Color.white.opacity(0.2))
+                        .padding(8)
                     
                     Capsule()
                         .fill(Color.white.opacity(0.2))
@@ -77,7 +82,7 @@ struct OnboardingView: View {
                     HStack{
                         Capsule()
                             .fill(Color("ColorRed"))
-                            .frame(width:80)
+                            .frame(width:buttonOffset + 80)
                         
                         Spacer()
                     }
@@ -95,13 +100,28 @@ struct OnboardingView: View {
                         }
                         .foregroundColor(.white)
                     .frame(width: 80, height: 80, alignment: .center)
-                    .onTapGesture {
-                        isOnboardingViewActive = false
-                    }
+                    .offset(x: buttonOffset)
+                    .gesture(
+                    DragGesture()
+                        .onChanged { gesture in
+                            if gesture.translation.width > 0 && buttonOffset <=
+                                buttonWidth - 80 {
+                                buttonOffset = gesture.translation.width
+                            }
+                        }
+                        .onEnded{ _ in
+                            if buttonOffset > buttonWidth / 2 {
+                                buttonOffset = buttonWidth - 80
+                                isOnboardingViewActive = false
+                            } else {
+                                buttonOffset = 0
+                            }
+                        }
+                    ) //: GESTURE
                     Spacer()
                     } //: HSTACK
                 }//: FOOTER
-                .frame(height: 80, alignment: .center)
+                .frame(width: buttonWidth, height: 80, alignment: .center)
                 .padding()
             } //: VSTACK
         } //: ZSTACK
